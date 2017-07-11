18eb7f2379b0b964bb99a119f21940eb6e977286
Ken Rowland <kenneth.rowland@lexisnexisrisk.com>
2017-07-11 10:58:02 -0400

    Working...

---
+++ configuration/config2/Type.hpp
@@ -0,0 +1,55 @@
+/*##############################################################################
+
+    HPCC SYSTEMS software Copyright (C) 2015 HPCC Systems®.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+############################################################################## */
+
+#ifndef _CONFIG2_TYPE_HPP_
+#define _CONFIG2_TYPE_HPP_
+
+#include "jiface.hpp"
+#include "jhash.ipp"
+#include "jutil.hpp"
+
+namespace CONFIG2
+{
+
+class CSettingType : public CInterface // TODO: what does CInterface get me?
+{
+  public:
+    CSettingTypoe(const std::string &name) : m_name(name){};
+    virtual ~CSettingType(){};
+
+    virtual bool checkValue(const std::string &value) { return (true); }
+
+  private:
+    std::string m_name;
+    CConstraints &m_constraints;
+};
+
+class CNotification : public CInterfaceOf<IInterface>
+{
+    friend class CNotificationManager;
+
+  public:
+    CNotification()
+    {
+    }
+    virtual ~CNotification()
+    {
+    }
+    StringArray m_NotificationMessagesArrays[eUnknown];
+};
+}
+#endif // _CONFIG2_TYPE_HPP_
