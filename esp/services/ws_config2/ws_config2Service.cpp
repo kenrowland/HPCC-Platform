cde50b53063d6972e6db0d45fdddd3ff517b3ecc
Ken Rowland <kenneth.rowland@lexisnexisrisk.com>
2017-09-14 16:06:48 -0400

    Updated ESP interface def and mock interface to reflect changes

--- esp/services/ws_config2/ws_config2Service.cpp
+++ esp/services/ws_config2/ws_config2Service.cpp
@@ -49,94 +49,92 @@ bool Cws_config2Ex::ongetPath(IEspContext &context, IEspGetPathRequest &req, IEs
 bool Cws_config2Ex::mockInterface(const std::string &path, IEspGetPathResponse &resp)
 {
     IArrayOf<IEspelementType> elements;
+    IArrayOf<IEspattributeType> attributes;
 
     if (path == ".")
     {
         Owned<IEspelementType> pElement = createelementType();
-        pElement->setName("hw");
-        pElement->setDisplayName("Hardware Configuration");
+        pElement->updateItemInfo().setName("hw");
+        pElement->updateItemInfo().setDisplayName("Hardware Configuration");
+        pElement->updateItemInfo().setStatus("ok");
         pElement->setClass("category");
         pElement->setPath(".hw");
         pElement->updateDoc().setTooltip("Defines the hardware configuration for the cluster");
         pElement->setNumChildren(5);
-        pElement->updateElementData().setNumAllowedInstances(0);
-        pElement->updateElementData().setNumRequiredInstances(0);
+        pElement->setNumAllowedInstances(0);
+        pElement->setNumRequiredInstances(0);
         elements.append(*pElement.getLink());
 
         pElement.setown(createelementType());
-        pElement->setName("sw");
-        pElement->setDisplayName("Software Configuration");
+        pElement->updateItemInfo().setName("sw");
+        pElement->updateItemInfo().setDisplayName("Software Configuration");
+        pElement->updateItemInfo().setStatus("ok");
         pElement->setClass("category");
         pElement->setPath(".sw");
         pElement->updateDoc().setTooltip("Defines the software configuration for the cluster");
         pElement->setNumChildren(4);
-        pElement->updateElementData().setNumAllowedInstances(0);
-        pElement->updateElementData().setNumRequiredInstances(0);
+        pElement->setNumAllowedInstances(0);
+        pElement->setNumRequiredInstances(0);
         elements.append(*pElement.getLink());
-
-        
     }
     else if (path == ".sw")
     {
         Owned<IEspelementType> pElement = createelementType();
-        pElement->setName("esp");
-        pElement->setDisplayName("ESP Processes");
+        pElement->updateItemInfo().setName("esp");
+        pElement->updateItemInfo().setDisplayName("ESP Processes");
+        pElement->updateItemInfo().setStatus("ok");
         pElement->setClass("instanceSet");
         pElement->setPath(".sw.esp");
         pElement->updateDoc().setTooltip("Defines ESP processes that run on various systems in the cluster");
         pElement->setNumChildren(2);
-        pElement->updateElementData().setNumAllowedInstances(0);
-        pElement->updateElementData().setNumRequiredInstances(1);
+        pElement->setNumAllowedInstances(0);
+        pElement->setNumRequiredInstances(1);
         elements.append(*pElement.getLink());
 
         pElement.setown(createelementType());
-        pElement->setName("dropzones");
-        pElement->setDisplayName("Dropzones");
+        pElement->updateItemInfo().setName("dropzones");
+        pElement->updateItemInfo().setDisplayName("Dropzones");
+        pElement->updateItemInfo().setStatus("ok");
         pElement->setClass("instanceSet");
         pElement->setPath(".sw.dropzones");
         pElement->updateDoc().setTooltip("Define dropzones for file transfer activities");
         pElement->setNumChildren(2);
-        pElement->updateElementData().setNumAllowedInstances(0);
-        pElement->updateElementData().setNumRequiredInstances(0);
+        pElement->setNumAllowedInstances(0);
+        pElement->setNumRequiredInstances(0);
         elements.append(*pElement.getLink());
 
         pElement.setown(createelementType());
-        pElement->setName("loggingagents");
-        pElement->setDisplayName("Loggin Agents");
+        pElement->updateItemInfo().setName("loggingagents");
+        pElement->updateItemInfo().setDisplayName("Logging Agents");
+        pElement->updateItemInfo().setStatus("ok");
         pElement->setClass("instanceSet");
         pElement->setPath(".sw.loggingagents");
         pElement->updateDoc().setTooltip("Define logging agents for use across ESP and other services");
         pElement->setNumChildren(0);
-        pElement->updateElementData().setNumAllowedInstances(0);
-        pElement->updateElementData().setNumRequiredInstances(0);
+        pElement->setNumAllowedInstances(0);
+        pElement->setNumRequiredInstances(0);
         elements.append(*pElement.getLink());
 
         pElement.setown(createelementType());
-        pElement->setName("xyzservice");
-        pElement->setDisplayName("XYZ Service");
+        pElement->updateItemInfo().setName("xyzservice");
+        pElement->updateItemInfo().setDisplayName("XYZ Service");
+        pElement->updateItemInfo().setStatus("ok");
         pElement->setClass("component");
         pElement->setPath(".sw.xyzservice");
         pElement->updateDoc().setTooltip("Configure XYZ service");
         pElement->setNumChildren(3);
-        pElement->updateElementData().setNumAllowedInstances(0);
-        pElement->updateElementData().setNumRequiredInstances(1);
+        pElement->setNumAllowedInstances(0);
+        pElement->setNumRequiredInstances(1);
         elements.append(*pElement.getLink());        
 
     }
     else if (path == ".sw.xyzservice")
     {
         Owned<IEspelementType> pElement = createelementType();
-        pElement->setName("attributes");
-        pElement->setDisplayName("Attributes");
-        pElement->setClass("valuesetSet");
-        pElement->setPath(".sw.xyzservice.attributes");
-        pElement->updateDoc().setTooltip("Attributes");
-        pElement->setNumChildren(3);
-        elements.append(*pElement.getLink());
 
-        pElement.setown(createelementType());
-        pElement->setName("otherstuff");
-        pElement->setDisplayName("Other Stuff");
+        pElement->updateItemInfo().setName("otherstuff");
+        pElement->updateItemInfo().setDisplayName("Other Stuff");
+        pElement->updateItemInfo().setStatus("ok");
         pElement->setClass("valueSet");
         pElement->setPath(".sw.xyzservice.otherstuff");
         pElement->updateDoc().setTooltip("Some other settings");
@@ -144,66 +142,112 @@ bool Cws_config2Ex::mockInterface(const std::string &path, IEspGetPathResponse &
         elements.append(*pElement.getLink());
 
         pElement.setown(createelementType());
-        pElement->setName("instances");
-        pElement->setDisplayName("System Bindings");
+        pElement->updateItemInfo().setName("instances");
+        pElement->updateItemInfo().setDisplayName("System Bindings");
+        pElement->updateItemInfo().setStatus("ok");
         pElement->setClass("instanceSet");
         pElement->setPath(".sw.xyzservice.instances");
         pElement->updateDoc().setTooltip("Computer bindings for the xyz service (5 allowed)");
         pElement->setNumChildren(0);  // current number defined (must compare with numXXXInstances below to see if can create or if any required)
-        pElement->updateElementData().setNumAllowedInstances(5);
-        pElement->updateElementData().setNumRequiredInstances(1);
+        pElement->setNumAllowedInstances(5);
+        pElement->setNumRequiredInstances(1);
         elements.append(*pElement.getLink());
-    }
-    else if (path == ".sw.xyzservice.attributes")
-    {
-        Owned<IEspelementType> pElement = createelementType();
-        pElement->setName("username");
-        pElement->setDisplayName("Username");
-        pElement->setClass("value");
-        pElement->updateDoc().setTooltip("Username for all operations");
-        pElement->updateElementData().updateType().setName("string");
-        pElement->updateElementData().updateType().updateLimits().setMin(5);
-        pElement->updateElementData().updateType().updateLimits().setMax(15);
+
+        Owned<IEspattributeType> pAttribute = createattributeType();
+        pAttribute->updateItemInfo().setName("username");
+        pAttribute->updateItemInfo().setDisplayName("Username");
+        pAttribute->updateItemInfo().setStatus("ok");
+        pAttribute->updateDoc().setTooltip("Username for all operations");
+        pAttribute->updateType().setName("string");
+        pAttribute->updateType().updateLimits().setMin(5);
+        pAttribute->updateType().updateLimits().setMax(15);
         StringArray excludeList;
         excludeList.append("username");
-        pElement->updateElementData().updateType().updateLimits().setDisallowList(excludeList);
-        pElement->updateElementData().setCurrentValue("bobtheuser");
-        pElement->updateElementData().setDefaultValue("username");
-        elements.append(*pElement.getLink());
+        pAttribute->updateType().updateLimits().setDisallowList(excludeList);
+        pAttribute->setCurrentValue("bobtheuser");
+        pAttribute->setDefaultValue("username");
+        attributes.append(*pAttribute.getLink());
 
-        pElement.setown(createelementType());
-        pElement->setName("password");
-        pElement->setDisplayName("Password");
-        pElement->setClass("value");
-        pElement->updateDoc().setTooltip("Password for the user");
-        pElement->updateElementData().updateType().setName("string");
-        pElement->updateElementData().updateType().updateLimits().setMin(5);
-        pElement->updateElementData().updateType().updateLimits().setMax(15);
+        pAttribute.setown(createattributeType());
+        pAttribute->updateItemInfo().setName("password");
+        pAttribute->updateItemInfo().setDisplayName("Password");
+        pAttribute->updateItemInfo().setStatus("ok");
+        pAttribute->updateDoc().setTooltip("User's password");
+        pAttribute->updateType().setName("string");
+        pAttribute->updateType().updateLimits().setMin(5);
+        pAttribute->updateType().updateLimits().setMax(15);
         StringArray modifiers;
-        modifiers.append("password");
-        pElement->updateElementData().updateType().setModifiers(modifiers);
-        pElement->updateElementData().setCurrentValue("mypassword");
-        pElement->updateElementData().setDefaultValue("");
-        elements.append(*pElement.getLink());
+        modifiers.append("mask");
+        modifiers.append("verify");
+        pAttribute->updateType().updateLimits().setDisallowList(excludeList);
+        attributes.append(*pAttribute.getLink());
 
-        pElement.setown(createelementType());
-        pElement->setName("maxopenfiles");
-        pElement->setDisplayName("Max Open Files");
-        pElement->setClass("value");
-        pElement->updateDoc().setTooltip("Password for the user");
-        pElement->updateElementData().updateType().setName("integer");
-        pElement->updateElementData().updateType().updateLimits().setMin(1);
-        pElement->updateElementData().updateType().updateLimits().setMax(20);
-        pElement->updateElementData().setCurrentValue("6");
-        pElement->updateElementData().setDefaultValue("3");
-        elements.append(*pElement.getLink());
+        pAttribute.setown(createattributeType());
+        pAttribute->updateItemInfo().setName("maxopenfiles");
+        pAttribute->updateItemInfo().setDisplayName("Max Open Files");
+        pAttribute->updateItemInfo().setStatus("ok");
+        pAttribute->updateDoc().setTooltip("Password for the user");
+        pAttribute->updateType().setName("integer");
+        pAttribute->updateType().updateLimits().setMin(1);
+        pAttribute->updateType().updateLimits().setMax(20);
+        pAttribute->setCurrentValue("6");
+        pAttribute->setDefaultValue("3");
+        attributes.append(*pAttribute.getLink());
+
 
 
     }
+    // else if (path == ".sw.xyzservice.attributes")
+    // {
+    //     Owned<IEspelementType> pElement = createelementType();
+    //     pElement->updateItemInfo().setName("username");
+    //     pElement->updateItemInfo().setDisplayName("Username");
+    //     pElement->setClass("value");
+    //     pElement->updateDoc().setTooltip("Username for all operations");
+    //     pElement->updateElementData().updateType().setName("string");
+    //     pElement->updateElementData().updateType().updateLimits().setMin(5);
+    //     pElement->updateElementData().updateType().updateLimits().setMax(15);
+    //     StringArray excludeList;
+    //     excludeList.append("username");
+    //     pElement->updateElementData().updateType().updateLimits().setDisallowList(excludeList);
+    //     pElement->updateElementData().setCurrentValue("bobtheuser");
+    //     pElement->updateElementData().setDefaultValue("username");
+    //     elements.append(*pElement.getLink());
+
+    //     pElement.setown(createelementType());
+    //     pElement->updateItemInfo().setName("password");
+    //     pElement->updateItemInfo().setDisplayName("Password");
+    //     pElement->setClass("value");
+    //     pElement->updateDoc().setTooltip("Password for the user");
+    //     pElement->updateElementData().updateType().setName("string");
+    //     pElement->updateElementData().updateType().updateLimits().setMin(5);
+    //     pElement->updateElementData().updateType().updateLimits().setMax(15);
+    //     StringArray modifiers;
+    //     modifiers.append("password");
+    //     pElement->updateElementData().updateType().setModifiers(modifiers);
+    //     pElement->updateElementData().setCurrentValue("mypassword");
+    //     pElement->updateElementData().setDefaultValue("");
+    //     elements.append(*pElement.getLink());
+
+    //     pElement.setown(createelementType());
+    //     pElement->updateItemInfo().setName("maxopenfiles");
+    //     pElement->updateItemInfo().setDisplayName("Max Open Files");
+    //     pElement->setClass("value");
+    //     pElement->updateDoc().setTooltip("Password for the user");
+    //     pElement->updateElementData().updateType().setName("integer");
+    //     pElement->updateElementData().updateType().updateLimits().setMin(1);
+    //     pElement->updateElementData().updateType().updateLimits().setMax(20);
+    //     pElement->updateElementData().setCurrentValue("6");
+    //     pElement->updateElementData().setDefaultValue("3");
+    //     elements.append(*pElement.getLink());
+
+
+    // }
 
 
     // just add the elements we made
     resp.setElements(elements); 
+    resp.setAttributes(attributes); 
     resp.setStatus("ok");
 
     return(true);
