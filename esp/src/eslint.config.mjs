6e98c250639244b48b616ec75e64ce3710dd51bc
Gordon Smith <GordonJSmith@gmail.com>
2026-03-27 13:31:14 +0000

    HPCC-36015 Enable set-state-in-effect linting checks
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- esp/src/eslint.config.mjs
+++ esp/src/eslint.config.mjs
@@ -73,7 +73,6 @@ export default defineConfig(
             "react-hooks/immutability": "off",
             "react-hooks/refs": "off",
             "react-hooks/purity": "off",
-            "react-hooks/set-state-in-effect": "off",
         },
     },
 )
